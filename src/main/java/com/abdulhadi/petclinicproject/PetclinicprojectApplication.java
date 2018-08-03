d7ee78dfa24aea9b21982f7d64303951230c7bac
abdulhadi1986 <36157744+abdulhadi1986@users.noreply.github.com>
2018-08-03 17:19:12 +0200

    project initialized

---
+++ src/main/java/com/abdulhadi/petclinicproject/PetclinicprojectApplication.java
@@ -0,0 +1,12 @@
+package com.abdulhadi.petclinicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicprojectApplication.class, args);
+    }
+}
